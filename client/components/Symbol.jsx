12474208fbe0df99c07b2b0cebe44c4831f831e7
Kyle Cameron <1kylecameron@gmail.com>
2017-02-09 18:54:18 -0500

    fixed minor bugs in app.jsx

--- client/components/Symbol.jsx
+++ client/components/Symbol.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Symbol = () => {
+const Symbol = function () {
   return (
     <div className='symbol'>
-      <img id='tophat' src='http://www.spore.com/static/avatar/500/231/078/black-top-hat.jpg' />
+      <img id='tophat' alt='' src='http://www.spore.com/static/avatar/500/231/078/black-top-hat.jpg' />
     </div>
   )
 }

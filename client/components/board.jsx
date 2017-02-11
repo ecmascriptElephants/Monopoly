d26f4b87132757aed19e7595694d0a2ec34787cb
RJ Mohammad <rjmohammad@outlook.com>
2017-02-11 15:06:03 -0500

    some changes

--- client/components/board.jsx
+++ client/components/board.jsx
@@ -1,30 +1,24 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Symbol from './Symbol'
 import DiceRoll from './dice_roll'
 
-const Board = () => {
-  return (
-    <div>
-      <DiceRoll />
-      <div className='board parent'>
-        <Symbol style={{'order': 0}} />
-        <div className='wire'>
-          <div className='flexcol'>
-            <div className='flexrow'>
-              <div className='top' />
-              <div className='item' />
-              <div className='item' />
-              <div className='item' />
-              <div className='item' />
-              <div className='item' />
-              <div className='item' />
-              <div className='item' />
-              <div className='item' />
-              <div className='item' />
-              <div className='top' />
-            </div>
-            <div className='flexmiddle'>
-              <div className='flexside'>
+class Board extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      dice: 0
+    }
+  }
+  render () {
+    return (
+      <div>
+        <DiceRoll />
+        <div className='board parent'>
+          <Symbol style={{ 'order': 0 }} />
+          <div className='wire'>
+            <div className='flexcol'>
+              <div className='flexrow'>
+                <div className='top' />
                 <div className='item' />
                 <div className='item' />
                 <div className='item' />
@@ -34,8 +28,34 @@ const Board = () => {
                 <div className='item' />
                 <div className='item' />
                 <div className='item' />
+                <div className='top' />
               </div>
-              <div className='flexside'>
+              <div className='flexmiddle'>
+                <div className='flexside'>
+                  <div className='item' />
+                  <div className='item' />
+                  <div className='item' />
+                  <div className='item' />
+                  <div className='item' />
+                  <div className='item' />
+                  <div className='item' />
+                  <div className='item' />
+                  <div className='item' />
+                </div>
+                <div className='flexside'>
+                  <div className='item' />
+                  <div className='item' />
+                  <div className='item' />
+                  <div className='item' />
+                  <div className='item' />
+                  <div className='item' />
+                  <div className='item' />
+                  <div className='item' />
+                  <div className='item' />
+                </div>
+              </div>
+              <div className='flexrow'>
+                <div className='top' />
                 <div className='item' />
                 <div className='item' />
                 <div className='item' />
@@ -45,26 +65,14 @@ const Board = () => {
                 <div className='item' />
                 <div className='item' />
                 <div className='item' />
+                <div className='top' />
               </div>
             </div>
-            <div className='flexrow'>
-              <div className='top' />
-              <div className='item' />
-              <div className='item' />
-              <div className='item' />
-              <div className='item' />
-              <div className='item' />
-              <div className='item' />
-              <div className='item' />
-              <div className='item' />
-              <div className='item' />
-              <div className='top'style={{'order': 0}} />
-            </div>
-          </div>
+          </div >
         </div >
       </div >
-    </div >
-  )
+    )
+  }
 }
 
 export default Board

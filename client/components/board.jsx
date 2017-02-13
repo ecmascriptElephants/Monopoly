830e195358a380a03655c25a3a494010667b8acd
RJ Mohammad <rjmohammad@outlook.com>
2017-02-13 10:45:01 -0500

    board settings

--- client/components/board.jsx
+++ client/components/board.jsx
@@ -1,21 +1,32 @@
 import React, { Component } from 'react'
 import Symbol from './Symbol'
-// import DiceRoll from './dice_roll'
+import DiceRoll from './dice_roll'
 import Player from './player'
 
 class Board extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      dice: 0
+      left: '96%',
+      top: '96%'
     }
+    this.dice = this.dice.bind(this)
   }
+
+  dice (value) {
+    var location = 96 - (8 * (value + 1))
+    this.setState({
+      left: `${location.toString()}%`
+    })
+  }
+
   render () {
     return (
       <div>
+        <DiceRoll dice={this.dice} />
         <Player name='RJ' piece='Hat' />
         <div className='board parent'>
-          <Symbol className='token' left='96%' top='96%' />
+          <Symbol className='token' left={this.state.left} top={this.state.top} />
           <div className='wire'>
             <div className='flexcol'>
               <div className='flexrow'>

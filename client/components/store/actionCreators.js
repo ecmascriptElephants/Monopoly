a54bcf3d4b13fb9aa0ca45b1f73cd1feea236659
Rana J Mohammad <rjmohammad@users.noreply.github.com>
2017-02-17 19:26:50 -0500

    Revert "Revert "Dev""

---
+++ client/components/store/actionCreators.js
@@ -0,0 +1,26 @@
+import { SET_GAME_ID,
+  SET_USERNAME,
+  SET_USER_ID,
+  SET_USERS_POSITIONS,
+  SET_INDEX
+} from './actions'
+
+export function setGameID (id) {
+  return {type: SET_GAME_ID, id}
+}
+
+export function setUsername (username) {
+  return {type: SET_USERNAME, username}
+}
+
+export function setUserID (userID) {
+  return {type: SET_USER_ID, userID}
+}
+
+export function setUserPositions (userPos, index) {
+  return {type: SET_USERS_POSITIONS, userPos, index}
+}
+
+export function setIndex (index) {
+  return {type: SET_INDEX, index}
+}

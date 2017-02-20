919e509b4aeae22392232109dfed81733444a940
RJ Mohammad <rjmohammad@outlook.com>
2017-02-20 16:47:06 -0500

    implemented toast notification

--- client/components/lobby.jsx
+++ client/components/lobby.jsx
@@ -4,6 +4,7 @@ import sock from '../helper/socket'
 import axios from 'axios'
 import { connect } from 'react-redux'
 import { setUsername, setGameID, setUserID, setMyIndex } from './store/actionCreators'
+import Toast from './toast'
 class Lobby extends Component {
   constructor (props) {
     super(props)
@@ -11,7 +12,9 @@ class Lobby extends Component {
       button: false,
       join: false,
       start: false,
-      messages: []
+      messages: [],
+      showToast: false,
+      comment: ''
     }
     axios.get('/user')
       .then((res) => {
@@ -35,7 +38,11 @@ class Lobby extends Component {
       this.props.dispatch(setMyIndex(data))
     })
     sock.socket.on('player joined', (data) => {
-      this.setState({ join: false, start: true })
+      this.setState({ join: false,
+        start: true,
+        showToast: true,
+        comment: 'Player joined'
+      })
       this.props.dispatch(setGameID(data.gameID))
     })
     sock.socket.on('send message', (data) => {
@@ -92,7 +99,7 @@ class Lobby extends Component {
           </ul>
           <input id='message' type='text' /><button onClick={this.submitMessage}>Send</button>
         </div>
-
+        <Toast message={this.state.comment} show={this.state.showToast} />
       </div>
     )
   }

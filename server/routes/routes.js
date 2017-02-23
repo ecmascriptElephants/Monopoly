37d9d77584bbcce51c1fd138311779833bd4dd69
Jinwei Lin <jinwei.lin8@gmail.com>
2017-02-23 14:06:45 -0500

    Revert "reset to routes files"

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,29 +1,35 @@
-const token = require('../jwt/jwt')
+let userController = require('../controllers/userController')
+let msgHistory = require('../controllers/msgHistoryController')
+
 module.exports = (app, express, passport) => {
   app.post('/signup', passport.authenticate('local-signup', {
-    failureRedirect: '/',
-    failureFlash: true
-  }),
-    (req, res) => {
-      res.send({ token: passport.token, user: passport.user })
-    })
+    failureRedirect: '/signup'
+  }), (req, res) => res.redirect('/board'))
 
-  app.post('/login', passport.authenticate('local-login', {
-    failureRedirect: '#/'
-  }),
-    (req, res) => {
-      res.send({ token: passport.token, user: passport.user })
-    })
+  // app.post('/login', (req, res, next) => {
+  //   console.log('log in a user',req.body)
+  // })
 
-  app.get('/auth/facebook', passport.authenticate('facebook'))
+  app.post('/login', (req, res, done) => {
+    passport.authenticate('local-login', (err, user, info) => {
+      if (err) return done(err)
+      if (!user) return res.redirect('/')
+      req.login(user, () => {
+        return res.redirect('/board')
+      })
+    })(req, res, done)
+  })
+
+  app.get('/auth/facebook', passport.authenticate('facebook'), (req, res) => {
+    console.log('should be in here')
+  })
 
   app.get('/auth/facebook/callback', passport.authenticate('facebook', {
     authType: 'rerequest',
     failureRedirect: '/login'
   }), (req, res) => {
     passport.user = req.user
-    passport.token = token.tokenGenerator(req.user.id)
-    res.redirect('/#/auth')
+    res.redirect('/lobby')
   })
 
   app.get('/logout', (req, res) => {
@@ -32,23 +38,29 @@ module.exports = (app, express, passport) => {
   })
 
   app.get('/', (req, res) => {
-    console.log('here')
     res.redirect('/#/')
   })
 
-  app.get('/auth', (req, res) => {
-    res.redirect('/#/auth')
-  })
-
-  app.get('/get-info', (req, res) => {
-    res.send({ token: passport.token, user: passport.user })
-  })
+  const ensureAuthenticated = (req, res, next) => {
+    if (req.isAuthenticated()) return next()
+    res.redirect('/login')
+  }
 
   app.get('/lobby', (req, res) => {
-    res.redirect('#/lobby')
+    res.redirect('/#/lobby')
   })
-
   app.get('/user', (req, res) => {
     res.send(passport.user)
+  })
+
+  app.post('/chats', (req, res) => {
+    let room = req.body.room
+    let keyword = req.body.keyword
+    let date = req.body.date
+    if (room === 'All Rooms') {
+      msgHistory.allHistory(keyword, res)
+    } else {
+      msgHistory.historyByQuery(room, keyword, res)
+    }
   })
 }

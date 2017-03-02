ed8b880ebedb831288de512030aa8343128d5ecd
RJ Mohammad <rjmohammad@outlook.com>
2017-03-01 21:08:31 -0500

    more issues

--- client/components/ToastHistory.jsx
+++ client/components/ToastHistory.jsx
@@ -1,9 +1,8 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import Toaster from './toast'
-import {Header, Container, Segment, Icon, Divider} from 'semantic-ui-react'
+import { Header, Container, Segment, Icon, Divider } from 'semantic-ui-react'
 import sock from '../helper/socket'
-
 
 class ToastHistory extends Component {
   constructor (props) {
@@ -13,31 +12,29 @@ class ToastHistory extends Component {
     }
   }
 
-
   handleMessage (message) {
     let comments = this.state.comments
     comments.push(message)
-    this.setState({comments: comments})
+    this.setState({ comments: comments })
     comments = this.state.comments.map((msg, i) => {
       return <li key={i}>{msg}</li>
     })
   }
   render () {
-<<<<<<< HEAD
     let comments = this.state.comments.map((comment, i) => {
       return <li key={i}>{comment}</li>
     })
     return (
       <Container className='toastHistory'>
-      <Segment raised vertical compact className='content'>
-      <Header as='h6' icon textAlign='center'>
-      <Header.Content>Activity Logs</Header.Content>
-      </Header>
-      <Divider />
-      <div>
-        {comments}
-      </div>
-      </Segment>
+        <Segment raised vertical compact className='content'>
+          <Header as='h6' icon textAlign='center'>
+            <Header.Content>Activity Logs</Header.Content>
+          </Header>
+          <Divider />
+          <div>
+            {comments}
+          </div>
+        </Segment>
       </Container>
     )
   }
